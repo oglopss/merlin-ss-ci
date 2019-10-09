a58613c46227bba5bc2c1cd1451a632151511441
oglopss <oglopss@gmail.com>
2019-10-09 09:35:09 -0700

    libsodium update from 1.0.11 to 1.0.18 for AEAD
    https://zzz.buzz/zh/gfw/2018/03/18/shadowsocks-xchacha20-ietf-poly1305-compatibility-notes/

--- .travis.sh
+++ .travis.sh
@@ -21,7 +21,7 @@ export ZLIB_VER=1.2.11
 export OPENSSL_VER=1.0.2k
 
 export PCRE_VER=8.43
-export LIBSODIUM_VER=1.0.11
+export LIBSODIUM_VER=1.0.18
 export MBEDTLS_VER=2.4.2
 export UDNS_VER=0.4
 export OBFS_VER=0.0.5
@@ -263,8 +263,11 @@ libsodium_build()
     #wget http://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
 
     # http://stackoverflow.com/questions/30418188/how-to-force-wget-to-overwrite-an-existing-file-ignoring-timestamp
-    wget --backups=1 https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VER/libsodium-$LIBSODIUM_VER.tar.gz
-
+    if [ "$LIBSODIUM_VER" == "1.0.18" ]; then 
+        wget --backups=1 https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VER-RELEASE/libsodium-$LIBSODIUM_VER.tar.gz
+    else
+        wget --backups=1 https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VER/libsodium-$LIBSODIUM_VER.tar.gz
+    fi
 
     tar xf libsodium-$LIBSODIUM_VER.tar.gz
     cd libsodium-$LIBSODIUM_VER
